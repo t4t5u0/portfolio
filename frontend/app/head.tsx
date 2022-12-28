413e71a63ec26eaefdf75bfa4499bd56fa36c6cc
t4t5u0 <ymmtryk0902@gmail.com>
2022-12-29 07:06:28 +0900

    Add: favicon

--- frontend/app/head.tsx
+++ frontend/app/head.tsx
@@ -2,6 +2,7 @@ const Head = () => {
   return (
     <>
       <title >Tatsuo&apos;s Portfolio</title>
+      <link rel="shortcut icon" href="boldt4t5u0_icon.ico" type="image/x-icon" />
     </>
   );
 };
